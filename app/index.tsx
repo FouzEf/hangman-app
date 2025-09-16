14a037fc0974689d0436213865d58893a4313687
fouz0062 <fouzifathii@gmail.com>
2025-09-16 12:03:53 +0200

    installed expo-linear gradient and used it as a component

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Text, View } from "react-native";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={['#80C2F3', '#C8E6C9']}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
+      style={Style.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+
+    <View style={Style.container}>
+      <Text>Hello</Text>
+      </View>
+
+      </LinearGradient>
+
   );
 }
+
+const Style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundImage: 'linear-gradient(180deg, #80C2F3 0%, #C8E6C9 100%)'
+  }
+})
+
