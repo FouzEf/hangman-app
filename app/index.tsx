c102832b062e95e5fd3c4736817acecea2e3a079
fouz0062 <fouzifathii@gmail.com>
2025-09-18 11:23:10 +0200

    updated the animation for the clouds

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,36 @@ import Home from "@/assets/images/HomeImage.png";
 import InvertedCloud from "@/assets/images/InvertedCloud.png";
 import SunWithCloud from "@/assets/images/SunWithCloud.png";
 import { LinearGradient } from "expo-linear-gradient";
-import { Image, StyleSheet, View } from "react-native";
+import { useEffect, useRef } from "react";
+import { Animated, Image, StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const cloud1X = useRef(new Animated.Value(0)).current;
+const cloud2X = useRef(new Animated.Value(0)).current;
+  const cloud3X = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+  const animateCloud = (cloudAnim: Animated.Value | Animated.ValueXY, toValue: number, duration: number) => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(cloudAnim, {
+          toValue,
+          duration,
+          useNativeDriver: true,
+        }),
+        Animated.timing(cloudAnim, {
+          toValue: 0,
+          duration,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+      }
+      animateCloud(cloud1X, 50, 6000);
+  animateCloud(cloud2X, -40, 8000);
+  animateCloud(cloud3X, 30, 7000);
+    }, [cloud1X, cloud2X, cloud3X]);
+  
   return (
     <LinearGradient
       colors={["#80C2F3", "#C8E6C9"]}
@@ -15,9 +42,9 @@ export default function Index() {
     >
       <View style={Style.container}>
         <Image source={SunWithCloud} style={Style.sun} />
-        <Image source={Cloud} style={Style.cloud1} />
-        <Image source={InvertedCloud} style={Style.cloud2} />
-        <Image source={InvertedCloud} style={Style.cloud3} />
+        <Animated.Image source={Cloud} style={[Style.cloud1,  { transform: [{ translateX: cloud1X }] }]} />
+        <Animated.Image source={InvertedCloud} style={[Style.cloud2,  { transform: [{ translateX: cloud2X }] }]} />
+        <Animated.Image source={InvertedCloud} style={[Style.cloud3,{ transform: [{ translateX : cloud3X }] }]} />
         <Image source={Home} style={Style.img} />
       </View>
     </LinearGradient>
